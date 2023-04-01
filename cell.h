29c2cedac1a0b158a87b1168b3f2251d9c521bfb
ParsaZa79 <pzeinali4@gmail.com>
2023-04-02 04:00:17 +0430

    Create cell.h and add fields and methods for the Cell class (subtype of Genome)

---
+++ cell.h
@@ -0,0 +1,25 @@
+#ifndef DNA_RNA_PROJECT_CELL_H
+#define DNA_RNA_PROJECT_CELL_H
+
+#include "genome.h"
+
+class Cell : public Genome
+{
+public:
+    Cell(int chromosomeCount, const std::vector<Genome> &chromosomes);
+
+    Cell();
+
+    bool apoptosis();
+    void bigMutation(const std::string &S1, int n, const std::string &S2, int m);
+    void subtleMutation(char n1, char n2, int n, int m);
+    void reverseMutation(const std::string &S1, int n);
+    std::vector<std::string> findComplementPalindromes(int n);
+
+private:
+    int chromosomeCount;
+    std::vector<Genome> chromosomes;
+};
+
+
+#endif //DNA_RNA_PROJECT_CELL_H
